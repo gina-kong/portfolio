575e832a41e6a83b49e862501ec4dacf9dfb3973
Gina Kong <ginagkong@gmail.com>
2025-01-19 15:58:33 +1100

    [chore]: abstract dock as component

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -1,75 +1,11 @@
-import { useRef } from 'react'
-import about from './assets/about.png'
-import contact from './assets/contact.png'
-import photos from './assets/photos.png'
-import projects from './assets/projects.png'
 import './App.css'
-import { scaleValue } from "./utils/scale";
-
-const maxAdditionalSize = 5;
+import { Dock } from "./features/Dock"
 
 function App() {
-  const dockRef = useRef(null);
-
-  const handleAppHover = (ev) => {
-    if (!dockRef.current) return;
-
-    const mousePosition = ev.clientX;
-    const iconPositionLeft = ev.currentTarget.getBoundingClientRect().left;
-    const iconWidth = ev.currentTarget.getBoundingClientRect().width;
-
-    const cursorDistance = (mousePosition - iconPositionLeft) / iconWidth;
-    const offsetPixels = scaleValue(
-      cursorDistance,
-      [0, 1],
-      [maxAdditionalSize * -1, maxAdditionalSize]
-    );
-
-    dockRef.current.style.setProperty(
-      "--dock-offset-left",
-      `${offsetPixels * -1}px`
-    );
-
-    dockRef.current.style.setProperty(
-      "--dock-offset-right",
-      `${offsetPixels}px`
-    );
-  };
 
   return (
     <>
-      <div className="page">
-        <div className="container">
-          <nav ref={dockRef} className="dock">
-            <ul>
-              <li className="app" onMouseMove={handleAppHover}>
-                <a href="#" target="_blank">
-                  <img src={about} className="logo" alt="about logo" />
-                  <span className="tooltip">About Me</span>
-                </a>
-              </li>
-              <li className="app" onMouseMove={handleAppHover}>
-                <a href="#" target="_blank">
-                  <img src={contact} className="logo" alt="contact logo" />
-                  <span className="tooltip">Contact</span>
-                </a>
-              </li>
-              <li className="app" onMouseMove={handleAppHover}>
-                <a href="#" target="_blank">
-                  <img src={photos} className="logo" alt="photos logo" />
-                  <span className="tooltip">Gallery</span>
-                </a>
-              </li>
-              <li className="app" onMouseMove={handleAppHover}>
-                <a href="#" target="_blank">
-                  <img src={projects} className="logo" alt="projects logo" />
-                  <span className="tooltip">Projects</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
+      <Dock />
     </>
   )
 }
